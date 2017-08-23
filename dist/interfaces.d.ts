a0b4eb55045f6950338df833c2003c2f891ad985
Simon Reymann <simon.reymann@gmail.com>
2017-08-23 13:22:15 +0200

    adapt interfaces

--- dist/interfaces.d.ts
+++ dist/interfaces.d.ts
@@ -1,9 +1,11 @@
 export interface IFrontendService {
     getFrontend(): Function;
-    transformRoute(routeConfig: any): any;
-    injectStaticLink(linkTag: {
-        type: string;
-        href: string;
-    }): any;
+    initialize(): void;
+}
+export interface IServerSideRenderService {
+    initialize(): void;
+    render(frontendProvider?: IFrontendProvider): Function;
+}
+export interface IFrontendProvider {
     initialize(): void;
 }
--- src/interfaces.ts
+++ src/interfaces.ts
@@ -1,9 +1,13 @@
 export interface IFrontendService {
   getFrontend(): Function;
-  transformRoute(routeConfig: any);
-  injectStaticLink(linkTag: {
-    type: string,
-    href: string
-  });
+  initialize(): void;
+}
+
+export interface IServerSideRenderService {
+  initialize(): void;
+  render(frontendProvider?: IFrontendProvider): Function;
+}
+
+export interface IFrontendProvider {
   initialize(): void;
 }
