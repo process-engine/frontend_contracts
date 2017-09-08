4bd1c39a390b3dac7435bd8427a665e604cc52ac
Heiko Mathes <heiko.mathes@gmail.com>
2017-09-08 17:24:03 +0200

    :fire: remove dist-folder

--- dist/interfaces.d.ts
+++
@@ -1,8 +0,0 @@
-export interface IFrontendService {
-    getFrontend(): Function;
-    initialize(): void;
-}
-export interface IServerSideRenderService {
-    initialize(frontendConfig: any): void;
-    render(res: any, req: any, next: any): Function;
-}
